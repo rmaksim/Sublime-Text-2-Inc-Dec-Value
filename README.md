52b2ea1e973aff9e4dc817de848522a53dfcfe04
rmaksim <razumenko.maksim@gmail.com>
2011-11-15 13:10:18 +0300

    added support for dates and strings

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 #Inc-Dec-Value
 
-## increase / decrease of numbers (integer and fractional), hex color values, and logical (or any, predetermined, opposing values) on the configured value
+## increase / decrease of numbers (integer and fractional),
+dates, hex color values, and logical (or any, predetermined, opposing values) on the configured value and a bonus - string actions (upper, lower, capitalize)
 
 ### Support Multiple Selections
 
@@ -8,56 +9,84 @@
 ![blame screenshot](https://github.com/rmaksim/Sublime-Text-2-Inc-Dec-Value/raw/master/inc_dec_hex_color.gif)
 ![blame screenshot](https://github.com/rmaksim/Sublime-Text-2-Inc-Dec-Value/raw/master/inc_dec_opposite.gif)
 ![blame screenshot](https://github.com/rmaksim/Sublime-Text-2-Inc-Dec-Value/raw/master/inc_dec_float.gif)
+![blame screenshot](https://github.com/rmaksim/Sublime-Text-2-Inc-Dec-Value/raw/master/inc_dec_dates.gif)
+![blame screenshot](https://github.com/rmaksim/Sublime-Text-2-Inc-Dec-Value/raw/master/inc_dec_strings.gif)
 
 
 Example of the correct values:
 ------------------------------
 
-  * positive and negative integer numbers
+  * ### positive and negative integer numbers
 
     => ... -2, -1, 0, 1, 2, ...
 
-  * positive and negative floating-point numbers
+
+  * ### positive and negative floating-point numbers
 
     => ... -1.1, -1.19, 0.119, 1.1119, 2.11119, ...
 
-  * positive and negative (integer and floating-point) numbers and any text after them
+    If value is floating-point - increases/decreases applies from last position.
+    for example 1.19 + `alt+up` = 1.20 and 1.11200 + `alt+down` = 1.11199
+
+
+  * ### positive and negative (integer and floating-point) numbers and any text after them
 
     => 12px, -5em, 100%, 42sometext, (24), [12, -13], {77: -88}, 0.1em, 62.5%/1.5
 
-  * hex colors
+
+  * ### hex colors
 
     => #123 #123456
 
-  * opposite values
+    Pressing the key `alt+up/down` increases/decreases
+    the one character to the left
+
+    If the cursor between the '#' and the hex number in the #123
+    the action applies to first character '1'
+
+    Pressing the key `super+up/down` increases/decreases
+    the total value of the hex color on +111/-111 or +111111/-111111
+
+
+  * ### dates in ISO format `YYYY-MM-DD`
+
+    => 2011-11-15
+
+    The increase in year / month / day is its own, without checking the validity of the resulting date.
+    For days, the value of loops between 1 and 31, for months - loops between 1 and 12.
+
+
+  * ### opposite values
 
     => true/false, True/False, TRUE/FALSE, left/right
 
+    Pressing the key `alt+up/down`
+    changes the value under the cursor ("true" or "false") to the opposite
 
-Pressing the key `alt+up/down` increases/decreases
-the one character to the left
 
-If value is floating-point - increases/decreases applies from last position.
-for example 1.19 + `alt+up` = 1.20 and 1.11200 + `alt+down` = 1.11199
+  * ### any string
 
-If the cursor between the '#' and the hex number in the #123
-- the action applies to first character '1'
+    => string String STRING
 
-Pressing the key `super+up/down` increases/decreases
-the total value of the hex color on +111/-111 or +111111/-111111
+    Pressing the key `alt+up` makes the first letter in the word in Uppercase (Capitalize).
 
-Pressing the key `alt+up/down`
-changes the value under the cursor ("true" or "false") to the opposite
+    Pressing the key `super+up` makes the word in UPPERCASE.
+
+    Pressing the key `alt+down` or `super+down` makes the word in lowercase.
+
+    **Important !**
+    There will be no change of words, which were applied different rules.
+    For example, the opposite of the values "true" will not be given to upper case, as a rule is applied to modify this value to "false".
 
 
 Not supported:
 --------------
 
-  * numbers in the text and after
+  * #### numbers in the text and after
 
     => qwe42asd, text42
 
-  * incorrect hex colors
+  * #### incorrect hex colors
 
     => #1 #12 #1234 #12345 #1234567...
 
--- inc_dec_value.py
+++ inc_dec_value.py
@@ -1,99 +1,229 @@
-import sublime, sublime_plugin, re
+import sublime, sublime_plugin, re, string, datetime
 
 class IncDecValueCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, delta):
+
+        self.edit = edit
+        self.delta = int(delta)
+
         for region in self.view.sel():
 
-            word_reg = self.view.word(region)
-            if not word_reg.empty():
+            self.region = region
+            self.word_reg = self.view.word(region)
 
-                inc_dec = int(delta)
+            if not self.word_reg.empty():
 
-                # expand region for test the previous symbol is "#" or ...
-                prev_sym_pos = word_reg.begin() - 1
-                prev_sym_reg = sublime.Region(prev_sym_pos, prev_sym_pos + 1)
+                self.apply_date()               or \
+                self.apply_hex_color()          or \
+                self.apply_floating_point()     or \
+                self.apply_integer()            or \
+                self.apply_opposite()           or \
+                self.apply_string()
 
-                # hex string
-                if self.view.substr(prev_sym_reg) == "#":
 
-                    # applies for one of hex numbers
-                    if delta in [1, -1]:
-                        # take the symbol to the left
-                        # if the cursor between '#' and the number - move it to the right
-                        if word_reg.begin() == region.begin():
-                            word_reg = sublime.Region(word_reg.begin(), region.end() + 1)
-                        else:
-                            word_reg = sublime.Region(region.begin() - 1, region.end())
+    #
+    # any date in ISO 8601 format, "YYYY-MM-DD", ex: 2011-11-14
+    #
+    def apply_date(self):
 
-                        word = self.view.substr(word_reg)
-                        match = re.compile('([0-9a-fA-F])').match(word)
+        word_reg = self.word_reg
+        word = self.view.substr(word_reg)
 
-                    else:
-                        inc_dec /= 10
-                        word = self.view.substr(word_reg)
-                        match = re.compile('([0-9a-fA-F]{3}([0-9a-fA-F]{3})?){1}$').match(word)
+        re_date = '([\d]{4}-[\d]{2}-[\d]{2})$'
 
-                    if match:
-                        new_word = ""
-                        for char in word:
-                            char = hex(int(char, 16) + inc_dec & 0xf)[2:]
-                            new_word += char
-                        self.view.replace(edit, word_reg, new_word)
+        if re.compile('([\d]{4})$').match(word): # check year
 
-                # simple the positive & negative numbers (integers & floating-point numbers)
+            tmp_reg = sublime.Region(word_reg.begin(), word_reg.begin() + 10)
+            word = self.view.substr(tmp_reg)
+
+            if re.compile(re_date).match(word): # date is valid -> year
+                # process with apply_integer
+                return False
+
+        elif re.compile('([\d]{2})$').match(word): # check month or day:
+
+            date_reg = sublime.Region(word_reg.begin() - 5, word_reg.begin() + 5)
+            date = self.view.substr(date_reg)
+
+            if re.compile(re_date).match(date): # date is valid -> month
+                self.view.replace(self.edit, word_reg, self.cycle(word, 12, self.delta))
+
+                return True
+
+            else: # check day
+                date_reg = sublime.Region(word_reg.begin() - 8, word_reg.begin() + 2)
+                date = self.view.substr(date_reg)
+
+                if re.compile(re_date).match(date): # date is valid -> day
+                    self.view.replace(self.edit, word_reg, self.cycle(word, 31, self.delta))
+
+                    return True
+
+
+    #
+    # cycle `word` between 1 and `max` value when adding `delta`
+    #
+    def cycle(self, word, max, delta):
+        return '%02d' % (((int(word) + max - 1 + delta) % max) + 1)
+
+
+    #
+    # any hex color, ex: #ee77ee; #f12; #f0e
+    #
+    def apply_hex_color(self):
+
+        word_reg = self.word_reg
+        word = self.view.substr(word_reg)
+
+        re_hex_color = '([0-9a-fA-F]{3}([0-9a-fA-F]{3})?){1}$'
+        match = re.compile(re_hex_color).match(word)
+
+        prev = self.prev()
+
+        if match and prev['sym'] == "#":
+
+            delta = self.delta
+
+            # applies for one of hex numbers
+            if delta in [1, -1]:
+                # take the symbol to the left
+                # if the cursor between '#' and the number - move it to the right
+                if word_reg.begin() == self.region.begin():
+                    word_reg = sublime.Region(word_reg.begin(), self.region.end() + 1)
                 else:
-                    # floating-point numbers
-                    if self.view.substr(prev_sym_reg) == ".":
-                        while True:
-                            prev_sym_pos -= 1
-                            prev_sym_reg = sublime.Region(prev_sym_pos, prev_sym_pos + 1)
-                            prev_sym = self.view.substr(prev_sym_reg)
-                            if not re.compile('(\d)').match(prev_sym):
-                                break
+                    word_reg = sublime.Region(self.region.begin() - 1, self.region.end())
 
-                        if self.view.substr(prev_sym_reg) != "-":
-                            prev_sym_pos += 1
+                word = self.view.substr(word_reg)
+                match = re.compile('([0-9a-fA-F])').match(word)
 
-                        prev_sym_reg = sublime.Region(prev_sym_pos, prev_sym_pos + 1)
+            else:
+                delta /= 10
+                word = self.view.substr(word_reg)
+                match = re.compile(re_hex_color).match(word)
 
-                        word_reg = sublime.Region(prev_sym_pos, word_reg.end())
-                        word = self.view.substr(word_reg)
-                        match = re.compile('(-*\d+\.(\d+))([a-zA-Z%]+)?').match(word)
+            if match:
+                new_word = ""
+                for char in word:
+                    char = hex(int(char, 16) + delta & 0xf)[2:]
+                    new_word += char
+                self.view.replace(self.edit, word_reg, new_word)
 
-                        if match:
-                            float_len = len(match.group(2))
-                            result = float(match.group(1)) + float(inc_dec) / (10 ** float_len)
-                            match3 = match.group(3) if match.group(3) else ""
-                            self.view.replace(edit, word_reg, ('%%0.%sf' % float_len) % result + match3)
+                return True
 
-                    # integers
-                    else:
 
-                        if self.view.substr(prev_sym_reg) == "-":
-                            word_reg = sublime.Region(prev_sym_pos, word_reg.end())
+    #
+    # any number of floating point, ex: 2.3mm or -0.27m
+    #
+    def apply_floating_point(self):
 
-                        word = self.view.substr(word_reg)
-                        match = re.compile('(-*[0-9]+)([a-zA-Z%]+)?').match(word)
+        prev = self.prev()
 
-                        if match:
-                            result = int(match.group(1)) + inc_dec
-                            match2 = match.group(2) if match.group(2) else ""
-                            self.view.replace(edit, word_reg, str(result) + match2)
+        # floating-point numbers
+        if prev['sym'] == ".":
+            while True:
+                prev = self.prev(prev['pos'] - 1)
+                if not re.compile('(\d)').match(prev['sym']):
+                    break
 
-                        # opposite values
-                        opp_values = [
-                            ("true", "false"),
-                            ("True", "False"),
-                            ("TRUE", "FALSE"),
-                            ("left", "right"),
-                        ]
-                        new_value = ''
-                        for k, v in opp_values:
-                            if k == word:
-                                new_value = v
-                            if v == word:
-                                new_value = k
+            if prev['sym'] != "-":
+                prev = self.prev(prev['pos'] + 1)
 
-                        if new_value:
-                            self.view.replace(edit, word_reg, new_value)
+            word_reg = sublime.Region(prev['pos'], self.word_reg.end())
+            word = self.view.substr(word_reg)
+            match = re.compile('(-*\d+\.(\d+))([a-zA-Z%]+)?$').match(word)
+
+            if match:
+                float_len = len(match.group(2))
+                result = float(match.group(1)) + float(self.delta) / (10 ** float_len)
+                match3 = match.group(3) if match.group(3) else ""
+                self.view.replace(self.edit, word_reg, ('%%0.%sf' % float_len) % result + match3)
+
+                return True
+
+
+    #
+    # any integer, ex: 12 123px; 123% 1em;
+    #
+    def apply_integer(self):
+
+        word_reg = self.word_reg
+        prev = self.prev()
+
+        if prev['sym'] == "-":
+            word_reg = sublime.Region(prev['pos'], word_reg.end())
+
+        word = self.view.substr(word_reg)
+        match = re.compile('(-*[0-9]+)([a-zA-Z%]+)?$').match(word)
+
+        if match:
+            result = int(match.group(1)) + self.delta
+            match2 = match.group(2) if match.group(2) else ""
+            self.view.replace(self.edit, word_reg, str(result) + match2)
+
+            return True
+
+
+    #
+    # any value from the list `opp_values`
+    #
+    def apply_opposite(self):
+
+        word = self.view.substr(self.word_reg)
+
+        opp_values = [
+            ("true", "false"),
+            ("True", "False"),
+            ("TRUE", "FALSE"),
+            ("left", "right"),
+        ]
+        new_value = ''
+        for k, v in opp_values:
+            if k == word:
+                new_value = v
+            if v == word:
+                new_value = k
+
+        if new_value:
+            self.view.replace(self.edit, self.word_reg, new_value)
+
+            return True
+
+
+    #
+    # any string
+    #
+    def apply_string(self):
+
+        word = self.view.substr(self.word_reg)
+        match = re.compile('([a-zA-Z]+)').match(word)
+
+        if match:
+            if   self.delta == 1:
+                fn = string.capitalize #capwords
+            elif self.delta == -1:
+                fn = string.lower
+            elif self.delta == 10:
+                fn = string.upper
+            elif self.delta == -10:
+                fn = string.lower
+
+            fn and self.view.replace(self.edit, self.word_reg, fn(word))
+
+            return True
+
+
+    #
+    # @return {String} Previous symbol
+    #
+    def prev(self, pos = None):
+
+        pos = pos or self.word_reg.begin() - 1
+        reg = sublime.Region(pos, pos + 1)
+        sym = self.view.substr(reg)
+
+        return {
+            'pos': pos,
+            'reg': reg,
+            'sym': sym
+        }
